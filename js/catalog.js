e7653c79c346d6c1cde1c8c6df43e5e7dc951bae
BehelitFam <mriodine@gmail.com>
2018-09-27 15:40:57 -0700

    yopyopyop

--- js/catalog.js
+++ js/catalog.js
@@ -80,7 +80,3 @@ catalogForm.addEventListener('submit', handleSubmit);
 // Before anything else of value can happen, we need to fill in the select
 // drop down list in the form.
 populateForm();
-
-var testItem = new Product('assets/banana.jpg', 'Banana');
-cart.addItem(testItem, 2);
-cart.saveToLocalStorage();
